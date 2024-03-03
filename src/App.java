68a60a8870e8802d20b3fc115bb718eef471d375
Samuel Levi <samuel.levicsdev@gmail.com>
2024-03-03 18:18:37 -0300

    Mais correcoes

--- src/App.java
+++ src/App.java
@@ -32,7 +32,7 @@ public class App {
         }
         leitor.close();
 
-        System.out.println("Playlist:");
+        System.out.println("Mpusicas da Playlist:");
         for (Musicas mu : lista_de_musicas) {
             System.out.format("\nNome: %s \nArtista: %s \nGênero: %s \nDuração: %.2f minutos. \n", mu.getNome(), mu.getArtista(), mu.getGenero(), mu.getDuracao());
         }
